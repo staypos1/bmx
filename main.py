d981a6af47d3570906a3df0ed0c2975b4440c9f7
rwe <mrowe.w@gmail.com>
2015-12-19 01:04:15 -0500

    Added bike as the player

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 # Init pygame module
 pygame.init()
 
+# TODO: Setup game variables
+
 # Setup screen variables
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
@@ -11,8 +13,8 @@ speed = [2, 2]
 white = (255, 255, 255)
 
 # Setup player variables
-box = pygame.image.load("box.png")
-boxrect = box.get_rect()
+bike = pygame.image.load("bikeright.png")
+bikerect = bike.get_rect()
 
 # Main game loop
 while True:
@@ -22,15 +24,15 @@ while True:
             sys.exit()
 
         # Move the player around the screen
-        boxrect = boxrect.move(speed)
+        bikerect = bikerect.move(speed)
 
         # Move the player based on users input
-        if boxrect.left < 0 or boxrect.right > width:
+        if bikerect.left < 0 or bikerect.right > width:
             speed[0] = -speed[0]
-        if boxrect.top < 0 or boxrect.bottom > height:
+        if bikerect.top < 0 or bikerect.bottom > height:
             speed[1] = -speed[1]
 
     # Create game window
     screen.fill(white)
-    screen.blit(box, boxrect)
+    screen.blit(bike, bikerect)
     pygame.display.flip()
