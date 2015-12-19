7b0a8a98dae526f0308736d7b9167e2936994922
rwe <mrowe.w@gmail.com>
2015-12-19 00:51:42 -0500

    Added movement to the player

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pygame.init()
 # Setup screen variables
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
-speed = (2, 2)
+speed = [2, 2]
 white = (255, 255, 255)
 
 # Setup player variables
@@ -20,6 +20,15 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+        # Move the player around the screen
+        boxrect = boxrect.move(speed)
+
+        # Move the player based on users input
+        if boxrect.left < 0 or boxrect.right > width:
+            speed[0] = -speed[0]
+        if boxrect.top < 0 or boxrect.bottom > height:
+            speed[1] = -speed[1]
 
     # Create game window
     screen.fill(white)
